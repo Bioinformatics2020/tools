8527d405c4f5a8108143c29827f38d7d27714326
unreal engine consumer <2252313806@qq.com>
2024-10-15 19:36:49 +0800

    修复计时精度为毫秒，提供循环功能实现示例

--- c++/CustomCoroutine.cpp
+++ c++/CustomCoroutine.cpp
@@ -5,26 +5,28 @@
 printf("TimeMs:%f  " fmt "\n", GetTimeMs(),__VA_ARGS__ ); \
 fflush(stdout)
 
-typedef void (*UserThreadFunc)(int);
+typedef void (*UserThreadFunc)(int, int);
 
-time_t GTimeStart;
+clock_t GTimeStart;
 
 float GetTimeMs()
 {
-    return difftime(time(NULL), GTimeStart) * 1000.0f;
+    return (double)(clock() - GTimeStart) / CLOCKS_PER_SEC * 1000.0;
 }
 
 UserThreadFunc GDelayTask[10];
 int GTaskBeginCode[10];
+int GTaskParam[10];
 float GTaskCallTime[10];
 int GTaskNum = 0;
 
-void AddTask(float EndTime, UserThreadFunc Fun, int NextCode)
+void AddTask(float EndTime, UserThreadFunc Fun, int NextCode, int Param)
 {
     if (GTaskNum >= 10)
         return;
     GDelayTask[GTaskNum] = Fun;
     GTaskBeginCode[GTaskNum] = NextCode;
+    GTaskParam[GTaskNum] = Param;
     GTaskCallTime[GTaskNum] = EndTime;
     GTaskNum++;
 }
@@ -38,14 +40,16 @@ void Yield()
     {
         if (NowTime >= GTaskCallTime[i])
         {
-            GDelayTask[i](GTaskBeginCode[i]);
+            GDelayTask[i](GTaskBeginCode[i], GTaskParam[i]);
             if (i < GTaskNum - 1)
             {
                 GDelayTask[i] = GDelayTask[GTaskNum - 1];
                 GTaskBeginCode[i] = GTaskBeginCode[GTaskNum - 1];
+                GTaskParam[i] = GTaskParam[GTaskNum - 1];
                 GTaskCallTime[i] = GTaskCallTime[GTaskNum - 1];
             }
             GTaskNum--;
+
             break;
         }
     }
@@ -56,29 +60,35 @@ int GetTaskNum()
     return GTaskNum;
 }
 
-void DelayMs(int DelayTime, UserThreadFunc Fun, int NextCode)
+void DelayMs(int DelayTime, UserThreadFunc Fun, int NextCode, int Param)
 {
-    float EndTime = GetTimeMs() + DelayTime;
-    AddTask(EndTime, Fun, NextCode);
+    float EndTime = GetTimeMs() + float(DelayTime);
+    AddTask(EndTime, Fun, NextCode, Param);
 }
 
 // Test
-#if 1 //函数实现方式
-void Task1(int BeginCode)
+#if 0 //函数实现方式
+void Task1(int BeginCode, int Param)
 {
     switch (BeginCode)
     {
     case 0:
         Print("Task1 0");
-        DelayMs(5000, Task1, 1);
+        DelayMs(1000, Task1, 1, 0);
         break;
     case 1:
-        Print("Task1 1");
-        DelayMs(5000, Task1, 2);
+        //模拟while循环
+        if (Param < 6)
+        {
+            Print("Task1 1 Param:%d", Param);
+            DelayMs(300, Task1, 1, Param + 1);
+            break;
+        }
+        DelayMs(1000, Task1, 2, 0);
         break;
     case 2:
         Print("Task1 2");
-        DelayMs(5000, Task1, 3);
+        DelayMs(5000, Task1, 3, 0);
         break;
     case 3:
         Print("Task1 3");
@@ -88,21 +98,28 @@ void Task1(int BeginCode)
     }
 }
 
-void Task2(int BeginCode)
+void Task2(int BeginCode, int Param)
 {
     switch (BeginCode)
     {
     case 0:
-        Print("Task2 0");
-        DelayMs(1000, Task2, 1);
+        //模拟do_While循环
+        Print("Task2 0 Param:%d", Param);
+        if (Param + 1 < 15)
+        {
+            DelayMs(400, Task2, 0, Param + 1);
+            break;
+        }
+
+        DelayMs(1000, Task2, 1, 0);
         break;
     case 1:
         Print("Task2 1");
-        DelayMs(5000, Task2, 2);
+        DelayMs(2000, Task2, 2, 0);
         break;
     case 2:
         Print("Task2 2");
-        DelayMs(5000, Task2, 3);
+        DelayMs(5000, Task2, 3, 0);
         break;
     case 3:
         Print("Task2 3");
@@ -121,42 +138,54 @@ break; \
 }}
 
 #define CAN_YIELD_FUNC(FunName) \
-void FunName(int BeginCode) \
+void FunName(int BeginCode, int Param) \
 { \
 switch (BeginCode) \
 { \
 case 0:
 
-#define DELAY_AND_YIELD(DelayTimeMs,FunName,NextCode) \
-DelayMs(DelayTimeMs, FunName, NextCode); \
+#define DELAY_AND_YIELD(DelayTimeMs,FunName,NextCode,Param) \
+DelayMs(DelayTimeMs, FunName, NextCode, Param); \
 break; \
 case NextCode:
 
 CAN_YIELD_FUNC(Task1)
     Print("Task1 0");
 
-    DELAY_AND_YIELD(5000, Task1, 1)
-    Print("Task1 1");
+    DELAY_AND_YIELD(1000, Task1, 1, 0)
+    //模拟while循环
+    if (Param < 6)
+    {
+        Print("Task1 1 Param:%d", Param);
+        DelayMs(300, Task1, 1, Param + 1);
+        return;
+    }
 
-    DELAY_AND_YIELD(5000, Task1, 2)
+    DELAY_AND_YIELD(5000, Task1, 2, 0)
     Print("Task1 2");
 
-    DELAY_AND_YIELD(5000, Task1, 3)
+    DELAY_AND_YIELD(5000, Task1, 3, 0)
     Print("Task1 3");
 
 CAN_YIELD_END
 
 
 CAN_YIELD_FUNC(Task2)
-    Print("Task2 0");
+    //模拟do_While循环
+    Print("Task2 0 Param:%d", Param);
+    if (Param + 1 < 15)
+    {
+        DelayMs(400, Task2, 0, Param + 1);
+        break;
+    }
 
-    DELAY_AND_YIELD(1000, Task2, 1)
+    DELAY_AND_YIELD(1000, Task2, 1, 0)
     Print("Task2 1");
 
-    DELAY_AND_YIELD(5000, Task2, 2)
+    DELAY_AND_YIELD(5000, Task2, 2, 0)
     Print("Task2 2");
 
-    DELAY_AND_YIELD(5000, Task2, 3)
+    DELAY_AND_YIELD(5000, Task2, 3, 0)
     Print("Task2 3");
 
 CAN_YIELD_END
@@ -164,10 +193,10 @@ CAN_YIELD_END
 
 int main(int argc, char *argv[])
 {
-    GTimeStart = time(NULL);
+    GTimeStart = clock();
 
-    Task1(0);
-    Task2(0);
+    Task1(0, 0);
+    Task2(0, 0);
     while (GetTaskNum())
     {
         Yield();
